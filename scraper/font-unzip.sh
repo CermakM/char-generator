2f42f02d3072a47f818ae5e114d28ede54519118
Marek Cermak <prace.mcermak@gmail.com>
2018-01-04 11:53:17 +0100

    scraper: written bash script to unzip all font.zip files
    Signed-off-by: Marek Cermak <prace.mcermak@gmail.com>
    
    new file:   scraper/font-unzip.sh

---
+++ scraper/font-unzip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Unzip all zip files found in the given path
+
+
+TARGET=$1
+
+[ -z $TARGET ] && printf '%s\n%s\n' 'argument TARGET has not been provided' \
+   'TARGET is a path to the parent font directory containing subdirectories with *.zip files' &&  exit 1
+
+find . -name '*.zip' -type f | xargs -I{} sh -c '\
+	font_file={}
+	font_dir=${font_file%/*}
+	pushd $font_dir >/dev/null
+	unzip ${font_dir##*/} 
+	popd >/dev/null'
+
