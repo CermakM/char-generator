6ad3a0961d730af1ae87bb3cc90cc8404c7aa06f
Marek Cermak <prace.mcermak@gmail.com>
2018-01-05 18:49:51 +0100

    Changed the source to https://www.ceskefonty.cz/
    - download only czech regular fonts
    - ask to continue if `fonts` exists
    - rename executable
    
    Signed-off-by: Marek Cermak <prace.mcermak@gmail.com>
    
    renamed:    font-scrape.sh -> font-scrape
    modified:   font-scrape.py
    modified:   make-exec.sh

--- scraper/make-exec.sh
+++ scraper/make-exec.sh
@@ -2,7 +2,7 @@
 
 [ -z `find . -name 'font-scrape.py'` ] && echo "MISSING font-scrape.py" 1>&2 && exit 1
 
-EXEC_FILE='font-scrape.sh'
+EXEC_FILE='font-scrape'
 
 { printf '#!/bin/env python3\n' && cat "font-scrape.py"; } > $EXEC_FILE
 
