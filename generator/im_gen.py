306dec5db80b9cd9ae05fb25d1699769ae480cec
Marek Cermak <prace.mcermak@gmail.com>
2018-01-07 00:39:26 +0100

    Change sample_size to 32 x 32
    Signed-off-by: Marek Cermak <prace.mcermak@gmail.com>
    
    deleted:    ../charset.txt
    modified:   im_gen.py

--- generator/im_gen.py
+++ generator/im_gen.py
@@ -4,31 +4,33 @@
 import os
 import sys
 
-from PIL import Image, ImageDraw, ImageFont, ImageOps
+from PIL import Image, ImageDraw, ImageFont
 
 # globals
 CHARSET_SIZE = 0
-IMAGE_DIR = os.environ.get('PONCO_IMAGE_DIR')
-FONT_DIR = os.environ.get('PONCO_FONT_DIR')
+# IMAGE_DIR = os.environ.get('PONCO_IMAGE_DIR')
+# FONT_DIR = os.environ.get('PONCO_FONT_DIR')
+
+FONT_DIR = '/home/macermak/code/thesis/char-generator/fonts'  # FIXME
+IMAGE_DIR = 'images'  # FIXME
 
 if not FONT_DIR or not IMAGE_DIR:
-    print("environment variable missing", file=sys.stderr)
+    print("FONT_DIR or IMAGE_DIR environment variable is missing", file=sys.stderr)
     exit(1)
 
-def get_correct_font(ttf, text, fit_size, font_size=20, eps=3):
-    import math
+
+def estimate_font_size(ttf, text, fit_size, font_size=20, eps=8):
     font = ImageFont.truetype(font=ttf, encoding='utf-8', size=font_size)
 
     w, h = font.getsize(text)
     _err = max(fit_size) - max(w, h)
     
-    while _err > eps:  # FIXME
+    while abs(_err) > eps:
         w, h = font.getsize(text)
         _err = max(fit_size) - max(w, h)
         
         font_size += 1 if _err > 0 else -1
-        font = ImageFont.truetype(font=ttf, encoding='utf-8',
-                size=int(font_size))
+        font = ImageFont.truetype(font=ttf, encoding='utf-8', size=int(font_size))
     
     return font
 
@@ -45,35 +47,32 @@ def load_char_set() -> list:
 
 def load_font_set() -> list:
     import re
-    files = os.listdir(FONT_DIR)
     fnames = []
     for root, _, files in os.walk(FONT_DIR):
-        fnames.extend(os.path.join(root, f) for f in files if
-                re.match(r'(.+)\.[odtfOTF]{3}', f))
+        fnames.extend(os.path.join(root, f) for f in files if re.match(r'(.+)\.[odtfOTF]{3}', f))
 
     return fnames
 
 
-def create_whiteboard(color='white', sample_size=(28, 28)) -> Image.Image:
-    import math
-
+def create_whiteboard(color='white', sample_size=(32, 32)) -> Image.Image:
     # a bit of a cheating here - charset size is 210 (15 x 14)
     bg_size = (15 * sample_size[0], 14 * sample_size[1])
 
     return Image.new(mode='RGBA', size=bg_size, color=color)
 
 
-def create_fontboard(charset, fontset):
+def create_fontboard(charset, fontset, sample_size=(32, 32)):
     from functools import reduce
     from numpy import subtract, floor_divide
     import random
     import re
 
-    sample_size = (28, 28)
-    board = create_whiteboard(sample_size=sample_size)
-    init_pos = (0, 0)
+    if not fontset:
+        print("Empty argument provided: fontset - ", fontset, file=sys.stderr)
 
-    for font_file in fontset:  # FIXME - for all fonts in the fontset
+    board = create_whiteboard(sample_size=sample_size)
+
+    for font_file in fontset:
         font_name = re.search(r"([^/]+)\.(\w)+$", font_file).group(1)
         board_name = "{path}/{ttf}-board.png".format(
                 path=IMAGE_DIR,
@@ -85,24 +84,25 @@ def create_fontboard(charset, fontset):
         print("Creating board", board_name, "...") 
         font_board = board.copy()
         try:
-            font = get_correct_font(
+            font = estimate_font_size(
                     ttf=font_file,
                     text='H',  # for sake of performance, assume that what works
                                # for H, works for everything else
-                    fit_size=(28, 28),
+                    fit_size=(32, 32),
                     eps=8
                     )
         except OSError as e:
-            print("Skipping", e.args)
+            print("Skipping", font_file, e.args)
             continue
 
         draw = ImageDraw.Draw(font_board)
 
+        init_pos = (0, 0)
         for char in charset:
             fo_x, fo_y = font.getoffset(char)
             # add a little bit of entropy
             font_offset = (
-                    fo_x + random.randint(1, 3),
+                    fo_x + random.randint(-3, 1),
                     fo_y + random.randint(1, 3))
             # location of char in a sample
             char_loc = reduce(subtract, (sample_size, font.size, font_offset))
@@ -125,7 +125,8 @@ def create_fontboard(charset, fontset):
         font_board.save(fp=board_name)
         print('Written', board_name)
 
-def generate_char_images(charset, size=(28, 28), *args, **kwargs):
+
+def generate_char_images(charset, sample_size=(32, 32), *args, **kwargs):
     pass
 
 
@@ -141,8 +142,8 @@ if __name__ == '__main__':
 
     charset = load_char_set()
     fontset = load_font_set()
+    im_size = (32, 32)
 
-    create_fontboard(charset, fontset)
+    create_fontboard(charset, fontset, sample_size=im_size)
 
-    im_size = (28, 28)
 
