3d0cca12f261ddc557d2525b59822df29409b59f
Marek Cermak <prace.mcermak@gmail.com>
2018-05-02 20:34:39 +0200

    Data augmentation module
    - apply random transformation to directory of images
    
    Signed-off-by: Marek Cermak <prace.mcermak@gmail.com>
    
    new file:   src/generator/data_augmentation.py
    new file:   tests/test_data_augmentation.py

---
+++ src/generator/data_augmentation.py
@@ -0,0 +1,128 @@
+"""Data augmentation module."""
+
+import argparse
+import os
+import sys
+import random
+from scipy import ndarray
+
+# image processing library
+from skimage import transform
+from skimage import util
+from skimage import io
+
+
+def random_rotation(image_array: ndarray):
+    """Pick a random degree of rotation between 25% on the left and 25% on the right."""
+    random_degree = random.uniform(-25, 25)
+    return transform.rotate(image_array, random_degree)
+
+
+def random_noise(image_array: ndarray):
+    """Add random noise to the image."""
+    return util.random_noise(image_array)
+
+
+def random_translation(image_array: ndarray):
+    """Apply random translation transformation to the image."""
+    tform = transform.SimilarityTransform(
+        translation=(random.randint(-3, 3), random.randint(-3, 3))
+    )
+    translated = transform.warp(image_array, tform)
+
+    return translated
+
+
+def random_warp(image_array: ndarray):
+    """Apply random warp transformation to the image."""
+    from math import pi
+    tform = transform.SimilarityTransform(
+        scale=1,
+        rotation=pi/4,
+        translation=(image_array.shape[0]/2, -100)
+    )
+
+    warped = transform.warp(image_array, tform)
+    return warped
+
+
+def apply_random_transformation(input_folder: str, output_folder: str, limit=None, img_type='png'):
+    """Load images from directory."""
+
+    available_transformations = [
+        random_rotation,
+        random_noise,
+        random_translation,
+        random_warp
+    ]
+
+    # find all files paths from the input_folder
+    image_files = [
+        os.path.join(input_folder, f) for f in os.listdir(input_folder)
+        if os.path.isfile(os.path.join(input_folder, f)) and f.endswith(img_type)
+    ]
+
+    limit = limit or len(image_files)
+
+    num_generated_files = 0
+    while num_generated_files < limit:
+        # random image from the input_folder
+        image_path = random.choice(image_files)
+        # read image as an two dimensional array of pixels
+        image_to_transform = io.imread(image_path, as_grey=True)
+        # random num of transformation to apply
+
+        transformation = random.choice(available_transformations)
+
+        # apply transformation
+        transformed_image = transformation(image_to_transform)
+
+        new_file_path = '%s/augmented_image_%s.jpg' % (output_folder, num_generated_files)
+
+        # write image to the disk
+        io.imsave(new_file_path, transformed_image)
+
+        num_generated_files += 1
+
+
+def parse_args(argv):
+    """Parse arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i', '--input-dir',
+        required=True,
+        help="Directory to look for images. NOTE: Does not recurse by default."
+    )
+    parser.add_argument(
+        '-o', '--output-dir',
+        default=os.path.abspath(__file__),
+        help="Output directory."
+    )
+    parser.add_argument(
+        '-t', '--format',
+        default='png',
+        help="Image format ('png' by default)."
+    )
+    parser.add_argument(
+        '-n', '--limit',
+        default=None,
+        help="Limit number of images to apply transformation to."
+    )
+
+    return parser.parse_args(argv)
+
+
+def main(argv):
+    """Run."""
+    args = parse_args(argv)
+
+    apply_random_transformation(
+        input_folder=args.input_dir,
+        output_folder=args.output_dir,
+        limit=args.limit,
+        img_type=args.format
+    )
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ tests/test_data_augmentation.py
@@ -0,0 +1,76 @@
+import os
+import tempfile
+import unittest
+
+from numpy import array_equal
+from skimage import io
+
+from src.generator import data_augmentation as daug
+
+
+TEST_DATA_ONES = os.path.abspath('test_data/49')
+TEST_DATA_THREES = os.path.abspath('test_data/51')
+
+
+class TestDataAugmentation(unittest.TestCase):
+    """Tests for data_augmentation module."""
+
+    def test_random_rotation(self):
+        """Test random rotation."""
+        images = [
+            io.imread(os.path.join(TEST_DATA_ONES, fp), as_grey=True)
+            for fp in os.listdir(TEST_DATA_ONES)
+        ]
+        img = images[0]
+        transformed_img = daug.random_rotation(img)
+
+        self.assertTrue(transformed_img.any())
+        self.assertFalse(array_equal(img, transformed_img))
+
+    def test_random_noise(self):
+        """Test random noise."""
+        images = [
+            io.imread(os.path.join(TEST_DATA_ONES, fp), as_grey=True)
+            for fp in os.listdir(TEST_DATA_ONES)
+        ]
+        img = images[0]
+        transformed_img = daug.random_noise(img)
+
+        self.assertTrue(transformed_img.any())
+        self.assertFalse(array_equal(img, transformed_img))
+
+    def test_random_translation(self):
+        """Test random translation."""
+        images = [
+            io.imread(os.path.join(TEST_DATA_ONES, fp), as_grey=True)
+            for fp in os.listdir(TEST_DATA_ONES)
+        ]
+        img = images[0]
+        transformed_img = daug.random_warp(img)
+
+        self.assertTrue(transformed_img.any())
+        self.assertFalse(array_equal(img, transformed_img))
+
+    def test_random_warp(self):
+        """Test random warp."""
+        images = [
+            io.imread(os.path.join(TEST_DATA_ONES, fp), as_grey=True)
+            for fp in os.listdir(TEST_DATA_ONES)
+        ]
+        img = images[0]
+        transformed_img = daug.random_warp(img)
+
+        self.assertTrue(transformed_img.any())
+        self.assertFalse(array_equal(img, transformed_img))
+
+    def test_apply_random_transform(self):
+        """Test random transformation."""
+        output_folder = tempfile.mkdtemp(prefix='test_', suffix='_augment')
+        limit = 5
+        daug.apply_random_transformation(
+            input_folder=TEST_DATA_ONES,
+            output_folder=output_folder,
+            limit=limit
+        )
+
+        self.assertEqual(len(os.listdir(output_folder)), limit)
